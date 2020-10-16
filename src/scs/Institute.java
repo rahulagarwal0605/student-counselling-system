e6dd5796b02d4e7a778b660878346dd626d6adbd
Rahul Agarwal <rahulagarwal0605@gmail.com>
2020-10-16 19:46:42 +0530

    update

---
+++ src/scs/Institute.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scs;
+
+/**
+ *
+ * @author rahul
+ */
+public class Institute {
+    int instituteId;
+    String instituteName;
+    String city;
+}
