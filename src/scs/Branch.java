38119e6aabc047c5d65ab10ff0d6164ae43bbcf0
Rahul Agarwal <rahulagarwal0605@gmail.com>
2020-10-16 19:33:27 +0530

    update

---
+++ src/scs/Branch.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scs;
+
+/**
+ *
+ * @author rahul
+ */
+public class Branch {
+    static int branchID;
+    String branchName;
+}
---
+++ src/scs/Student.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scs;
+import java.util.*;
+
+/**
+ *
+ * @author rahul
+ */
+public class Student {
+    String name;
+    Date dob;
+    int rollNum;
+    int rank;
+    String Quota;
+    char gender;
+    String category;
+    Branch Preferences;
+}
