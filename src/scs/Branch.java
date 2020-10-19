ef2bb074eb63515a08cfe67ce47014752c07f99d
Rahul Agarwal <rahulagarwal0605@gmail.com>
2020-10-19 16:38:22 +0530

    update

--- src/scs/Branch.java
+++ src/scs/Branch.java
@@ -4,14 +4,35 @@
  * and open the template in the editor.
  */
 package scs;
+import java.util.Scanner;
+import java.sql.*;
 
 /**
  *
  * @author rahul
  */
 public class Branch {
-    static int branchID;
+    int branchID;
     String branchName;
     
+    Branch(int id, String name) {
+        this.branchID = id;
+        this.branchName = name;
+    }
+
+    static void addBranch() {
+        int tempID;
+        String tempName;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter BranchID");
+        tempID = sc.nextInt();
+        tempName = sc.next();
+        Branch tempBranch = new Branch(tempID, tempName);
+        
+    }
+    
+    static void addBranch(Branch b){
+        
+    }
     
 }
--- src/scs/Main.java
+++ src/scs/Main.java
@@ -4,11 +4,56 @@
  * and open the template in the editor.
  */
 package scs;
+import java.sql.*;
+import java.util.Scanner;
 
 /**
  *
  * @author rahul
  */
 public class Main {
+    static String user, pass;
+    static Scanner sc = new Scanner(System.in);
     
+    static {
+        user = "root";
+        pass = "root";
+    }
+    
+    static void initDB() {
+        
+    }
+    
+    static void conDB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.print("Enter Username : ");
+            user = sc.next();
+            System.out.print("Enter Password : ");
+            pass = sc.next();
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306",user,pass);  
+            Statement stmt = con.createStatement();  
+            ResultSet rs = stmt.executeQuery("show databases");
+            boolean flag = false;
+            while(rs.next()) {
+                if(rs.getString(1).equals("scs")) {
+                    flag = true;
+                }
+            }
+            if(flag) {
+                System.out.println("Database already created!");
+            }
+            else {
+                System.out.println("Creating Database...");
+                stmt.execute("create database scs");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Student Counselling System");
+        conDB();
+    }
 }
